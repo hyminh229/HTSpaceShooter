6a5b57d686d1f7adfcf73cd7ed943802675b6d86
gg <thinh290305@gmail.com>
2026-08-28 03:22:57 +0700

    Add BulletDestroyZone

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,8 +4,23 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] private float speed = 10f;
 
+
     private void Update()
     {
         transform.Translate(Vector2.up * speed * Time.deltaTime);
     }
+
+    private void Awake()
+    {
+        BulletDestroyZone bulletDestroyZone = GetComponent<BulletDestroyZone>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision2D)
+    {
+        if (collision2D.gameObject.TryGetComponent(out BulletDestroyZone bulletDestroyZone))
+        {
+            Destroy(gameObject);
+        }
+    }
+
 }
---
+++ Assets/Scripts/BulletDestroyZone.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class BulletDestroyZone : MonoBehaviour
+{
+    
+}
